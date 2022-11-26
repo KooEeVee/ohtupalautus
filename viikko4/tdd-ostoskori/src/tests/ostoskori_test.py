01f9d1b2a8239c0afcdd1e0e889f38c7b8871570
Kaisa <45688258+KooEeVee@users.noreply.github.com>
2022-11-26 21:46:24 +0200

    ostoskorissa 1 tavara ostoksen nimi ja määrä test

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -51,4 +51,13 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(len(self.kori.ostokset()), 1)
 
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        nimi, maara = self.kori.ostokset()[0]
+        self.assertEqual(nimi, "Maito")
+        self.assertEqual(maara, 1)
+
+
+
     
